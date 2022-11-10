7ba4f656acb53157bca1c23373fa896d9ad7477c
mbhuttor <90875996+mbhuttor@users.noreply.github.com>
2022-11-10 13:56:49 -0500

    Create error.h

---
+++ error.h
@@ -0,0 +1,14 @@
+#ifndef PS5_ERROR_H
+#define PS5_ERROR_H
+
+#define SUCCESS       0
+#define NULLPTR       1
+#define TOOMANY       2
+#define TOOFEW        3
+#define UNKNOWN       4
+
+extern char errorMessages[][256];
+
+
+
+#endif //PS5_ERROR_H
