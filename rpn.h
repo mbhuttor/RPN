ef898e35a570fe28e8e2595dcca66977e5b8346e
mbhuttor <90875996+mbhuttor@users.noreply.github.com>
2022-11-10 14:14:55 -0500

    Create rpn.h

---
+++ rpn.h
@@ -0,0 +1,15 @@
+
+//
+// Created by marya on 4/23/2021.
+//
+
+#ifndef PS5_RPN_H
+#define PS5_RPN_H
+
+#include "stack.h"
+double evaluate(char* expression, int* status);
+
+
+
+
+#endif //PS5_RPN_H
