41f41ade02ba13b1906e432286c7abab28f12a24
mbhuttor <90875996+mbhuttor@users.noreply.github.com>
2022-11-10 13:58:34 -0500

    Create infix.h

---
+++ infix.h
@@ -0,0 +1,12 @@
+
+//
+// Created by marya on 4/23/2021.
+//
+
+#ifndef PS5_INFIX_H
+#define PS5_INFIX_H
+#include "stack.h"
+void infix_to_rpn(char* expression, char *output, int* status);
+
+
+#endif //PS5_INFIX_H
