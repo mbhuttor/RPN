c0559696bd76189dcb4f3bf6f1192314fe11f171
mbhuttor <90875996+mbhuttor@users.noreply.github.com>
2022-11-10 13:56:12 -0500

    Create error.c

---
+++ error.c
@@ -0,0 +1,15 @@
+
+
+//
+// Created by marya on 4/22/2021.
+//
+
+#include "error.h"
+char errorMessages[][256] = {
+        "Calculation successful.",
+        "Nullptr error",
+        "Too many operands",
+        "Two few operands",
+        "Unknown Operand"
+};
+
